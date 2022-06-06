9ad26e22d0a395cc29c4de8c5e367813181b32f3
Guy Tayou <guy.tayou@gmail.com>
2022-06-06 22:42:39 +0200

    use PostgreSQL as database when using docker-compose (#5)

--- flight-service/app/database.py
+++ flight-service/app/database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.settings import SQLALCHEMY_DATABASE_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
